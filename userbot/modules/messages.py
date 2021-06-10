f13ba1cc382d47fdda15eb5d0b40af884b9019cb
waly <85253290+jackblose@users.noreply.github.com>
2021-06-11 04:39:13 +0700

    Update messages.py

--- userbot/modules/messages.py
+++ userbot/modules/messages.py
@@ -59,7 +59,7 @@ async def purgeme(delme):
 
     smsg = await delme.client.send_message(
         delme.chat_id,
-        "`Berhasil Menghilangkan Jejak Chatsex,` " + str(count) + " `Jejak Chatsex telah terhapus`",
+        "`Berhasil Menghilangkan Jejak Pawang,` " + str(count) + " `Jejak Pawang telah terhapus`",
     )
     """
     if BOTLOG:
