1d8bc9b2cc27d4c90170dc8da2bf8de278b58f17
waly <85253290+jackblose@users.noreply.github.com>
2021-06-11 04:36:52 +0700

    Update kenalan.py

--- userbot/modules/kenalan.py
+++ userbot/modules/kenalan.py
@@ -2,15 +2,33 @@ from time import sleep
 from userbot.events import register
 
 
-@register(outgoing=True, pattern='^.r(?: |$)(.*)')
+@register(outgoing=True, pattern='^.w(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
-    await typew.edit("`Hai Perkenalkan Namaku Ramadhani`")
+    await typew.edit("`Hai kenalin namaku waly`")
     sleep(3)
-    await typew.edit("`19 Tahun`")
+    await typew.edit("`21 Tahun`")
     sleep(1)
-    await typew.edit("`Tinggal Di jakarta, Salam Kenal:)`")
+    await typew.edit("`Profesi sebagai`")
+    sleep(1)
+    await typew.edit("`Beban Keluarga`")
+    sleep(1)
+    await typew.edit("`Negara`")
+    sleep(1)
+    await typew.edit("`Asia`")
+    sleep(1)
+    await typew.edit("`Dunia`")
+    sleep(1)
+    await typew.edit("`Angkasa`")
+    sleep(1)
+    await typew.edit("`Antariksa`")
+    sleep(1)
+    await typew.edit("`Dan antar kita😑`")
+    sleep(1)
+    await typew.edit("`bjir malah curhat`")
+    sleep(1)
+    await typew.edit("`Tinggal Di Aceh, Salam Kenal:)`")
 # Create by myself @localheart
 
 
