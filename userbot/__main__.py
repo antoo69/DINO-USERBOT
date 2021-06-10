91660a5cdaaeb48253b684d6a669f42d71a84e8b
waly <85253290+jackblose@users.noreply.github.com>
2021-06-11 04:11:23 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"RAM-UBOT ㊙️ V7.0 [SUDAH AKTIF YA ASU!]")
+    f"JACKBOT 🥂 V7.0 [SUDAH AKTIF YA GANTENG!]")
 
 
 if len(argv) not in (1, 3, 4):
